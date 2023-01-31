6f74063943f2e4f732328399d1eee48f21b6e598
hernannicolaspereyra <116227131+hernannicolaspereyra@users.noreply.github.com>
2023-01-31 18:04:38 -0300

    primer commit

---
+++ curriculum_vitae_JS.js
@@ -0,0 +1,15 @@
+document.getElementById('boton_facebook').onclick = function(){
+    document.getElementById('Contacto').innerHTML = "https://www.facebook.com/nicolas.pereyra.12764"
+}
+
+document.getElementById('boton_telefono').onclick = function(){
+    document.getElementById('Contacto').innerHTML = "4270-0906"
+}
+
+document.getElementById('boton_celular').onclick = function(){
+    document.getElementById('Contacto').innerHTML = "15-5947-7349"
+}
+
+document.getElementById('boton_mail').onclick = function(){
+    document.getElementById('Contacto').innerHTML = "hernannicolaspereyra@hotmail.com"
+}
